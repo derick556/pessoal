39bc5cd6e274cd972f74fa36dc6f3ccb4acdc1cc
derick556 <derick_chaves@estudante.sesisenai.org.br>
2025-05-14 11:21:48 +0000

    update

---
+++ Lista 1/atividade12.py
@@ -0,0 +1,12 @@
+# Recebe dois números
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+
+
+# Calcula a divisão
+resultado = n1 / n2
+
+# Mostra o resultado
+print("A divisão do primeiro pelo segundo é", resultado)
+98
